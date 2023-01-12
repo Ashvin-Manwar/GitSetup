4307d25fdf67f6482574f809bbcfcd75650d5e28
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-01-12 13:15:56 +0530

    ..Update

---
+++ dec13.java
@@ -0,0 +1,15 @@
+class a{
+    int num=0;// 1 =>2=> 3
+ int increment(){
+    int nectNum=num++;
+    return nextNum;
+}
+public static void main(String[] args) {
+    a obj1=new a();
+    a obj2=new a();
+    System.out.println(obj1.increment());
+    System.out.println(obj1.increment());
+    System.out.println(obj1.increment());
+}
+
+ }
---
+++ dec13.js
@@ -0,0 +1,23 @@
+// function a(){
+//  let num =0;
+//  function increment(){
+//     num++;
+//     return num;
+//    }   
+//    return increment;
+// }
+
+// let fn1=a();
+// let fn2=a();
+// console.log(fn1());
+// console.log(fn1());
+// console.log(fn2());
+let a=[1,2,3,4,5,6,7,8,9];
+
+a.function (el){
+    if(el% 2 ===1)
+    odd.push(el);
+} 
+}
+}
+
