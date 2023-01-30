2dac1c060b60687389bfdc48c748778a2c84541e
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-01-30 12:42:51 +0530

    NAMASTE

--- NAMASTE/startJS.js
+++ NAMASTE/startJS.js
@@ -93,10 +93,149 @@
 // // z()// >7
 
 //lec 11 Closures time
-function x(){
-    var i=1
-    setTimeout(function(){
-        console.log(i)
-    },3000)
-}
-x()
+// function x(){
+//     var i=1
+//     setTimeout(function(){
+//         console.log(i)
+//     },3000)
+// }
+// x()
+
+// function x(){
+//     for(let i=1;i<=5;i++){
+//     setTimeout(function(){
+//         console.log(i)
+//     },i*1000)//1 2 3 4 5 output
+//    }
+// }
+// x()
+// function x(){
+//     for(let i=1;i<=5;i++){
+//         function close(i){
+//          setTimeout(function(){
+//           console.log(i)
+//          },i*1000)
+//        }
+//        close(i)
+//     }
+// }
+// x()
+
+//lec 12
+// function outer(){
+//     var a=10
+//     function inner(){
+//         console.log(a)
+//     }
+//     return inner
+// }
+// // outer()()
+// var close=outer()
+// close()
+
+// function outer(b){
+//     function inner(){
+//         console.log(a,b)// 10 Hello world
+//     }
+//     let a=10
+//     return inner
+// }
+// // outer()()
+// var close=outer("Hello World")
+// close()
+
+// function outest(){
+//     var c=20
+// function outer(b){
+//     function inner(){
+//         console.log(a,b,c)
+//     }
+//     let a=10
+//     return inner
+// }
+// return outer
+// }
+// // outer()()
+// var close=outest()("Hello World")
+// close()
+
+//hiding data
+// function counter(){
+//     var count=0
+//     return function incrementCounter(){
+//         count++
+//         console.log(count)
+//     }
+// }
+// var counter1=counter()//acces the counter
+// counter1()
+// counter1()
+
+// var counter2=counter()
+// counter2()
+// counter2()
+// counter2()
+
+// function Counter(){
+//         var count=0
+//        this.incrementCounter=function(){
+//             count++
+//             console.log(count)
+//         }
+//         this.decrementCounter=function(){
+//             count--
+//             console.log(count)
+//         }
+//     }
+//     var counter1=new Counter()
+//     counter1.incrementCounter()
+//     counter1.incrementCounter()
+//     counter1.decrementCounter()
+
+// lec 13 Anonymous Function
+
+//Function Statement aka Function Declaration
+// function a(){
+//     console.log("a called")
+// }
+
+//Function Expression
+
+// var b=function (param1,param2){
+//     console.log("b called")
+// }
+// a()
+// b()
+// xyz()
+
+//Anonymous Function
+// function(){
+// }
+    
+// const b=function(param1){
+
+//     return function xyz(){
+
+//     }
+// }
+//  console.log(b())
+
+//Named Function Expression
+//Difference btw Parameter & Argument ?
+// First Class Function
+// Arrow Function
+
+//lec 14 call back
+//what is call back function in js
+setTimeout(function(){
+    console.log("timer")
+},5000)
+ function x(y){
+    console.log("x")
+    y()
+ }
+ x(function y(){
+    console.log("y")
+ })
+
+ //JS is a synchronous an dsingle threded language
