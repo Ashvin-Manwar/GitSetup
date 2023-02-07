2900a2a69886f642749ea794302993b6a21b9316
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-07 21:42:19 +0530

    Creative

--- newtonclasspractice/Class Inheritance.js
+++ newtonclasspractice/Class Inheritance.js
@@ -1,36 +1,23 @@
-
-
-
-  // import using require
-
-// declare class
 var Shape = require('./shape.js');
-// declare class
+
 class Circle extends Shape {
      constructor(color) {
           super();
      }
      calculateArea() {
-
      }
 }
-
 // export class using module.exports
 module.exports = Circle;
-
-
 // declare class
 class Shape {
      constructor() {
           this.color = "green";
      }
      drawShape() {
-
      }
      calculateArea() {
-
      }
 }
-
 // export class using module.exports
 module.exports = Shape;
