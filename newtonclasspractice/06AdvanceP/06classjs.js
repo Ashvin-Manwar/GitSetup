cdcafd2832bc6877758bae73d3128c95b771f1bf
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-12 23:29:04 +0530

    class

--- newtonclasspractice/06AdvanceP/06classjs.js
+++ newtonclasspractice/06AdvanceP/06classjs.js
@@ -35,10 +35,10 @@ class SubAdmin extends User {
 module.exports = User;
 
 const rock = new User("rock", "rock@rock.com");
-//console.log(rock.getInfo());
+console.log(rock.getInfo());
 rock.enrollCourse("Angular Bootcamp");
-// console.log(rock.getCourseList());
-// console.log(rock.courseList);
+console.log(rock.getCourseList());
+console.log(rock.courseList);
 
 const tom = new SubAdmin("tom", "tom@jerry.com");
 console.log(tom.getAdminInfo());
