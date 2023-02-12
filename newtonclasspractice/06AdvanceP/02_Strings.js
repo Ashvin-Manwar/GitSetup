a625fdf70e26a0d5d4d22c1134cbd54764094cd7
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-12 09:37:06 +0530

    Strings

---
+++ newtonclasspractice/06AdvanceP/02_Strings.js
@@ -0,0 +1,8 @@
+var name = `ashvin`;
+var fullName = `${name} Manwar`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("ashvin"));
+console.log(fullName.toUpperCase());
