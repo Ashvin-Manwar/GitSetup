bcac9e73369ce7cac35fbb672696251b9f3bd7b2
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-12 23:34:47 +0530

    async

---
+++ newtonclasspractice/06AdvanceP/08async.js
@@ -0,0 +1,19 @@
+const uno = () => {
+    console.log("I am One");
+  };
+  
+  const dos = () => {
+    setTimeout(() => {
+      console.log("Wooohoooo");
+    }, 3000);
+    console.log("I am Two");
+  };
+  
+  const tres = () => {
+    console.log("I am Three");
+  };
+  
+  uno();
+  dos();
+  tres();
+  
